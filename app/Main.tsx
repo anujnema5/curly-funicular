237ff937dc08727b7b4d09e2908b78a0926c08c5
Anuj nema <anujnema63@gmail.com>
2023-09-01 10:43:18 +0530

    Image integration

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,6 +2,7 @@ import Link from '@/components/Link'
 import Category from '@/components/Category'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
+import Image from 'next/image'
 // import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const MAX_DISPLAY = 5
@@ -11,7 +12,7 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
             Latest Songs
           </h1>
           <p className="text-lg leading-7 lg:w-4/5 text-gray-500 dark:text-gray-400">
@@ -21,19 +22,28 @@ export default function Home({ posts }) {
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, category } = post
+            const { slug, date, title, summary, category, images } = post
+            
             return (
               <li key={slug} className="py-12">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
+                  <div className={"flex flex-col sm:flex-row gap-7 items-center justify-center px-1"}>
+                    {/* <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                       </dd>
-                    </dl>
+                    </dl> */}
+                    {images.length !==0 ? 
+                    <Image src={images[0]} className='sm:w-72 sm:h-52 rounded-md' alt='thumbnail' aria-label='Thumnail Image for Post' width={1000} height={1000}/> : <div className='sm:w-72 sm:h-52 w-full h-56 rounded-md bg-gray-800/50 flex items-center justify-center text-gray-400'>Insert Image</div>}
                     <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
+                      <div className="space-y-3">
+                      <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                      </dd>
+                    </dl> 
                         <div>
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
                             <Link
@@ -43,7 +53,7 @@ export default function Home({ posts }) {
                               {title}
                             </Link>
                           </h2>
-                          <div className="flex flex-wrap">
+                          <div className="flex sm:mt-2 mt-4 flex-wrap">
                             {category.map((category) => (
                               <Category key={category} text={category} />
                             ))}
