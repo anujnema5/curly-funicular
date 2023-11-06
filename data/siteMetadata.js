f56d5c649dbba2ef9f173f71d9a7de5deecedcc8
anujnema <anujnema63@gmail.com>
2023-11-06 14:31:21 +0530

    rter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   author: 'ChordsCo',
   headerTitle: 'ChordsCo',
   description:
-    'Learn popular and lesser known songs on guitar, ukulele and piano. Get accurate and easy-to-follow chords notations',
+    'Learn popular and lesser known songs on guitar, ukulele and piano. Get accurate and easy-to-follow chords notations.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://www.chordsco.com',
